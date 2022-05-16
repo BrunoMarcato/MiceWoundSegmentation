73b9f6198199d1ad5f6404e370cee455d45af874
Bruno Marcato <bruno11prog@outlook.com>
2022-05-16 18:43:24 -0300

    att models

--- models/models.py
+++ models/models.py
@@ -2,19 +2,18 @@
 FCN models file
 '''
 
-from configparser import Interpolation
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import regularizers, initializers, Input, Model
-from tensorflow.keras.layers import MaxPool2D, Conv2D, Conv2DTranspose, Lambda, Dropout, Add
+from tensorflow.keras.layers import MaxPool2D, Conv2D, Conv2DTranspose, Lambda, Dropout, Add, UpSampling2D
 
-def vgg16(l2=0, dropout=0):
+def vgg16(weight_decay=0, dropout=0.5):
     '''
     VGG16 network
     
     args:
-        l2 = L2 regularization factor (float), l2=0 by default
-        dropout = dropout rate (float), dropout=0 by default
+        weight_decay = L2 regularization factor (float), weight_decay=0 by default
+        dropout = dropout rate (float), dropout=0.5 by default
         classes = number of classes
     return:
         Keras model
@@ -23,23 +22,19 @@ def vgg16(l2=0, dropout=0):
     ##Input as keras tensor
     input = Input(shape=(None, None, 3), name='input')
 
-    ##Preprocessing - should only be loaded in the same environment where they were saved
-    
-    #x = Lambda(tf.keras.applications.vgg16.preprocess_input, name='preprocessing')(input)
-
     ##Block 1 - 64 filters
     x = Conv2D(filters = 64,
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv1-1')(input)
 
     x = Conv2D(filters=64,
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv1-2')(x)
 
     x = MaxPool2D(pool_size=(2,2),
@@ -51,14 +46,14 @@ def vgg16(l2=0, dropout=0):
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv2-1')(x)
 
     x = Conv2D(filters=128,
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv2-2')(x)
     
     x = MaxPool2D(pool_size=(2,2),
@@ -70,21 +65,21 @@ def vgg16(l2=0, dropout=0):
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv3-1')(x)
 
     x = Conv2D(filters=256,
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv3-2')(x)
 
     x = Conv2D(filters=256,
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv3-3')(x)
 
     x = MaxPool2D(pool_size=(2,2),
@@ -96,21 +91,21 @@ def vgg16(l2=0, dropout=0):
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv4-1')(x)
 
     x = Conv2D(filters=512,
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv4-2')(x)
 
     x = Conv2D(filters=512,
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv4-3')(x)
 
     x = MaxPool2D(pool_size=(2,2),
@@ -122,21 +117,21 @@ def vgg16(l2=0, dropout=0):
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv5-1')(x)
 
     x = Conv2D(filters=512,
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv5-2')(x)
 
     x = Conv2D(filters=512,
                 kernel_size=(3,3),
                 padding='same',
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='Conv5-3')(x)
 
     x = MaxPool2D(pool_size=(2,2),
@@ -150,7 +145,7 @@ def vgg16(l2=0, dropout=0):
                 strides=(1,1), 
                 padding='same', 
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2), 
+                kernel_regularizer=regularizers.L2(l2=weight_decay), 
                 name='conv6')(x)
 
     x = Dropout(rate=dropout, name='drop-conv6')(x)
@@ -160,7 +155,7 @@ def vgg16(l2=0, dropout=0):
                 strides=(1,1), 
                 padding='same', 
                 activation='relu',
-                kernel_regularizer=regularizers.L2(l2=l2), 
+                kernel_regularizer=regularizers.L2(l2=weight_decay), 
                 name='conv7')(x)
 
     x = Dropout(rate=dropout, name='drop-conv7')(x)
@@ -169,14 +164,14 @@ def vgg16(l2=0, dropout=0):
 
 
 
-def fcn32(vgg16, l2=0):
+def fcn32s(vgg16, weight_decay=0):
     '''
     32x upsampled
     
     Args:
         vgg16: VGG16 model
         fcn16: FCN16 model
-        l2 = L2 regularization factor (float), l2=0 by default
+        weight_decay = L2 regularization factor (float), weight_decay=0 by default
 
     returns:
         keras model
@@ -187,42 +182,44 @@ def fcn32(vgg16, l2=0):
                 strides=(1,1), 
                 padding='same', 
                 activation='linear',
-                kernel_regularizer=regularizers.L2(l2=l2),
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
                 name='score-conv7')(vgg16.get_layer('drop-conv7').output)
 
-    x = Conv2DTranspose(filters=21, 
-                        kernel_size=(64,64), 
-                        strides=(32, 32), 
-                        padding='same', 
-                        use_bias=False, 
-                        activation='softmax', 
-                        #kernel_initializer=BilinearInterpolation(), 
-                        kernel_regularizer=regularizers.l2(l2=l2), 
-                        name='FCN32s')(x)
+    x = UpSampling2D(size=(32,32), name='upsample-32')(x)
+
+    x = Conv2D(filters=21, 
+                kernel_size=(1,1),
+                strides=(1,1),
+                padding='same',
+                activation='linear',
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
+                name='FCN32s')(x)
 
     return Model(vgg16.input, x)
 
 
 
-def fcn16(vgg16, fcn32, l2=0):
+def fcn16s(vgg16, fcn32, weight_decay=0):
     '''
     16x upsampled 
     
     Args:
         vgg16: VGG16 custom keras model
         fcn32: FCN32 custom keras model
-        l2 = L2 regularization factor (float), l2=0 by default
+        weight_decay = L2 regularization factor (float), weight_decay=0 by default
 
     returns:
         keras model
     '''
-    x = Conv2DTranspose(filters=21, 
-                        kernel_size=(4,4), 
-                        strides=(2,2), 
-                        padding='same', 
-                        activation='linear', 
-                        kernel_regularizer=regularizers.l2(l2=l2),
-                        name='upsampled-conv7')(vgg16.get_layer('score-conv7').output)
+    x = UpSampling2D(size=(2,2))(vgg16.get_layer('drop-conv7').output)
+
+    x = Conv2D(filters=21, 
+                kernel_size=(1,1),
+                strides=(1,1),
+                padding='same',
+                activation='linear',
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
+                name='upsample-conv7')(x)
 
     y = Conv2D(filters=21, 
                 kernel_size=(1,1), 
@@ -230,63 +227,66 @@ def fcn16(vgg16, fcn32, l2=0):
                 padding='same',  
                 activation='linear',
                 kernel_initializer=initializers.Zeros(), #Net starts with unmodified predictions
-                kernel_regularizer=regularizers.l2(l2=l2) 
+                kernel_regularizer=regularizers.l2(l2=weight_decay) 
                 )(vgg16.get_layer('Pool4').output)
 
-    m = Add(name='fusion4')([x,y]) ##fusion
+    m = Add(name='step4')([x,y]) ##fusion
 
-    m  = Conv2DTranspose(filters=21, 
-                            kernel_size=(32,32), 
-                            strides=(16,16), 
-                            padding='same', 
-                            activation='softmax',
-                            use_bias=False, 
-                            #kernel_initializer=BilinearInterpolation(), 
-                            kernel_regularizer=regularizers.l2(l2=l2), 
-                            name='FCN16s')(m)
+    m  = UpSampling2D(size=(16,16), name='FCN16s')(m)
+
+    x = Conv2D(filters=21, 
+                kernel_size=(1,1),
+                strides=(1,1),
+                padding='same',
+                activation='linear',
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
+                name='FCN16s')(m)
 
     return Model(fcn32.input, m)
 
     
 
-def fcn8(vgg16, fcn16, l2=0):
+def fcn8s(vgg16, fcn16, weight_decay=0):
     '''
     8x upsampled
     
     Args:
         vgg16: VGG16 custom keras model
         fcn16: FCN16 custom keras model
-        l2 = L2 regularization factor (float), l2=0 by default
+        weight_decay = L2 regularization factor (float), weight_decay=0 by default
 
     returns:
         keras model
     '''
 
-    x = Conv2DTranspose(filters=21, 
-                        kernel_size=(4,4), 
-                        strides=(2,2), 
-                        padding='same',
-                        activation='linear', 
-                        kernel_regularizer=regularizers.l2(l2=l2), 
-                        name='upsampled-fusion')(fcn16.get_layer('fusion4').output)
+    x = UpSampling2D(size=(2,2), name='upsampled-step4')(fcn16.get_layer('step4').output)
+
+    x = Conv2D(filters=21, 
+                kernel_size=(1,1),
+                strides=(1,1),
+                padding='same',
+                activation='linear',
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
+                name='upsample-step4')(x)
 
     y = Conv2D(filters=21, 
                 kernel_size=(1,1), 
                 strides=(1,1), 
                 padding='same', 
                 activation='linear', 
-                kernel_regularizer=regularizers.l2(l2=l2), 
+                kernel_regularizer=regularizers.l2(l2=weight_decay), 
                 )(vgg16.get_layer('Pool3').output)
 
-    m = Add(name='fusion3')([x,y])
+    m = Add(name='step3')([x,y])
 
-    m = Conv2DTranspose(filters=21, 
-                        kernel_size=(16,16), 
-                        strides=(8,8), 
-                        padding='same', 
-                        activation='softmax', 
-                        #kernel_initializer=BilinearInterpolation(),
-                        kernel_regularizer=regularizers.l2(l2=l2), 
-                        name='FCN-8s')(m)
+    m = UpSampling2D(size=(8,8), name='upsampled-step4')(fcn16.get_layer('step4').output)
+
+    m = Conv2D(filters=21, 
+                kernel_size=(1,1),
+                strides=(1,1),
+                padding='same',
+                activation='linear',
+                kernel_regularizer=regularizers.L2(l2=weight_decay),
+                name='upsample-step4')(m)
 
     return Model(fcn16.input, m)
