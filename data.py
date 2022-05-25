f3668297a105c4710895fc5a8623cf3d4bd9f67e
Bruno Marcato <bruno11prog@outlook.com>
2022-05-25 13:40:01 -0300

    shuffle for cross validation

--- data.py
+++ data.py
@@ -1,25 +1,8 @@
 import os
 import numpy as np
-import tensorflow as tf
-from shutil import copy2
 
-def split_dataset(BASE_PATH='split_data', DATASET_PATH='rats_data', len_train=50, len_val=26):
-    os.makedirs(BASE_PATH, exist_ok=True)
-
-    #creating train/val folders
-    train_dir = os.path.join(BASE_PATH, 'train')
-
-    val_dir = os.path.join(BASE_PATH, 'validation')
-
-    dirs = ['images', 'labels']
-    for dir in dirs:
-        os.makedirs(os.path.join(train_dir, dir))
-        os.makedirs(os.path.join(val_dir, dir))
-    del dirs
-
-    #copying files from original folder to dataset folder
-    images = []
-    labels = []
+def split_dataset(DATASET_PATH='rats_data', holdout=80):
+    pair = []
 
     treatments = os.listdir(DATASET_PATH)
     for treatment in treatments:
@@ -29,8 +12,9 @@ def split_dataset(BASE_PATH='split_data', DATASET_PATH='rats_data', len_train=50
                 animals = os.listdir(os.path.join(DATASET_PATH, treatment, day))
                 for animal in animals:
                     path = os.path.join(DATASET_PATH, treatment, day, animal)
-                    images.append(os.path.join(path, animal+'.png'))
-                    labels.append(os.path.join(path, animal+'_label.png'))
+                    tupla = os.path.join(path, animal+'.png'), os.path.join(path, animal+'_label.png')
+                    pair.append(tupla)
+
         else:
             doses = os.listdir(os.path.join(DATASET_PATH, treatment))
             for dose in doses:
@@ -39,22 +23,34 @@ def split_dataset(BASE_PATH='split_data', DATASET_PATH='rats_data', len_train=50
                     animals = os.listdir(os.path.join(DATASET_PATH, treatment, dose, day))
                     for animal in animals:
                         path = os.path.join(DATASET_PATH, treatment, dose, day, animal)
-                        images.append(os.path.join(path, animal+'.png'))
-                        labels.append(os.path.join(path, animal+'_label.png'))
+                        tupla = os.path.join(path, animal+'.png'), os.path.join(path, animal+'_label.png')
+                        pair.append(tupla)
 
-    if len(images) != len_train + len_val:
-        size = len_train + len_val
-        len_train = size*3//4
-        len_val = size//4
-        print(f'Invalid size for train/val. New train: {len_train}. New Val: {len_val}')
-        del size
+    #shuffle for cross validation
+    np.random.seed(np.random.randint(0, 1000000))
+    np.random.shuffle(pair)
+    
+    train_dataset = []
+    test_dataset = []
 
+    for item in range(len(pair)):
+        if item < len(pair)*holdout//100:
+            train_dataset.append(pair[item])
+        else:
+            test_dataset.append(pair[item])
 
-    #distributing images and labels at train/val datasets
-    for image, label in zip(images[:len_train], labels[:len_train]):
-        copy2(image, os.path.join(BASE_PATH, 'train', 'images'))
-        copy2(label, os.path.join(BASE_PATH, 'train', 'labels'))
+    del pair
 
-    for image, label in zip(images[len_train:len_train+len_val], labels[len_train:len_train+len_val]):
-        copy2(image, os.path.join(BASE_PATH, 'validation', 'images'))
-        copy2(label, os.path.join(BASE_PATH, 'validation', 'labels'))
+    # #distributing images and labels at train/val datasets
+    X_train, X_test, y_train, y_test = [], [], [], []
+    for i in range(len(train_dataset)):
+        X_train.append(train_dataset[i][0])
+        y_train.append(train_dataset[i][1])
+
+    for i in range(len(test_dataset)):
+        X_test.append(test_dataset[i][0])
+        y_test.append(test_dataset[i][1])
+
+    del train_dataset, test_dataset
+
+    return X_train, X_test, y_train, y_test
